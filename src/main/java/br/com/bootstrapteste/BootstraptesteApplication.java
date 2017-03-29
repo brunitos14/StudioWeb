927e88701be97b3150bbec9d4ee9a24affe49c42
cielo <cielo@STFALXC035162-W>
2017-03-29 15:39:38 -0300

    teste

--- src/main/java/br/com/bootstrapteste/BootstraptesteApplication.java
+++ src/main/java/br/com/bootstrapteste/BootstraptesteApplication.java
@@ -1,18 +1,22 @@
-//package br.com.bootstrapteste;
-//
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//
-//@SpringBootApplication
-//public class BootstraptesteApplication {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(BootstraptesteApplication.class, args);
-//	}
+package br.com.bootstrapteste;
 
-//	@Bean
-//	public WebSecurityConfigurerAdapter webSecurityConfigurerAdapter(){
-//		return new WebSecurityConfig();
-//	}
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-//}
+import br.com.bootstrapteste.conf.WebSecurityConfig;
+
+@SpringBootApplication
+public class BootstraptesteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootstraptesteApplication.class, args);
+	}
+
+	@Bean
+	public WebSecurityConfigurerAdapter webSecurityConfigurerAdapter(){
+		return new WebSecurityConfig();
+	}
+
+}
