a8ca775002aca55dc9049112361aa782452ceb26
cielo <cielo@STFALXC035162-W>
2017-03-29 15:06:50 -0300

    teste

--- src/main/java/br/com/bootstrapteste/BootstraptesteApplication.java
+++ src/main/java/br/com/bootstrapteste/BootstraptesteApplication.java
@@ -1,18 +1,18 @@
-package br.com.bootstrapteste;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class BootstraptesteApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BootstraptesteApplication.class, args);
-	}
+//package br.com.bootstrapteste;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class BootstraptesteApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(BootstraptesteApplication.class, args);
+//	}
 
 //	@Bean
 //	public WebSecurityConfigurerAdapter webSecurityConfigurerAdapter(){
 //		return new WebSecurityConfig();
 //	}
 
-}
+//}
--- src/main/java/br/com/bootstrapteste/ServletInitializer.java
+++ src/main/java/br/com/bootstrapteste/ServletInitializer.java
@@ -1,13 +1,13 @@
-package br.com.bootstrapteste;
-
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.web.support.SpringBootServletInitializer;
-
-public class ServletInitializer extends SpringBootServletInitializer {
-
-	@Override
-	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(BootstraptesteApplication.class);
-	}
-
-}
+//package br.com.bootstrapteste;
+//
+//import org.springframework.boot.builder.SpringApplicationBuilder;
+//import org.springframework.boot.web.support.SpringBootServletInitializer;
+//
+//public class ServletInitializer extends SpringBootServletInitializer {
+//
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(BootstraptesteApplication.class);
+//	}
+//
+//}
