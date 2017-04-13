05b38053fb85855517d73ea8c080fc6d106e05e7
cielo <cielo@STFALXC035162-W>
2017-04-13 10:11:26 -0300

    teste chamada rest

--- src/main/java/br/com/bootstrapteste/service/UserService.java
+++ src/main/java/br/com/bootstrapteste/service/UserService.java
@@ -4,6 +4,10 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.WebTarget;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
@@ -22,7 +26,7 @@ import br.com.bootstrapteste.VOs.UserVO;
 
 @Service
 public class UserService implements UserDetailsService {
-	
+
 	@Value("${controllerservices}")
     private String rest;
 
@@ -54,22 +58,26 @@ public class UserService implements UserDetailsService {
 
 	 private String getUser(String email)
 		{
-		    final String uri = rest + "account/";
+//		    final String uri = rest + "account/{user}";
 
-		    RestTemplate restTemplate = new RestTemplate();
-		    HttpHeaders headers = new HttpHeaders();
-		    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
-		    headers.add("user", email);
+		    Client client = ClientBuilder.newClient();
+		    WebTarget target = client.target(rest);
+		    String conteudo = target.path("account/" + email).request().get(String.class);
 
-		    final Map<String, Object> param = new HashMap<>();
-	        param.put("user", email);
+//		    RestTemplate restTemplate = new RestTemplate();
+//		    HttpHeaders headers = new HttpHeaders();
+//		    headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+//		    headers.add("user", email);
+//
+//		    final Map<String, Object> param = new HashMap<>();
+//	        param.put("user", email);
+//
+//	        final HttpEntity<?> entity = new HttpEntity<Object>(param, headers);
+//
+//		    ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
 
-	        final HttpEntity<?> entity = new HttpEntity<Object>(param, headers);
+		    System.out.println(conteudo);
 
-		    ResponseEntity<String> result = restTemplate.exchange(uri, HttpMethod.GET, entity, String.class);
-
-		    System.out.println(result);
-
-		    return result.getBody();
+		    return conteudo;
 		}
 }
